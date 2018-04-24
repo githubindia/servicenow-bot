4f6820e9969df895b6b8b19743f0e5b1ec98f761
githubindia <shubham08gupta08@gmail.com>
2018-04-24 14:55:01 +0530

    passport corrected

--- dispatcher/getAuth.js
+++ dispatcher/getAuth.js
@@ -4,7 +4,7 @@ var accessToken;
 module.exports = {
     "getAuth": function(request, response) {
         var session = request.session;
-        if(session && session.passport && session.pssport.user.accessToken) {
+        if(session && session.passport && session.passport.user.accessToken) {
             accessToken = session.passport.user.accessToken;
             response.redirect('https://www.messenger.com/closeWindow/?display_text=Authenticated');
         }
